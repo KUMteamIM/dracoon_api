ca02784d534411b2a149f6c8c346fd91e627a0c6
Pradeep Jerome <prjerome@gmail.com>
2021-06-01 10:11:48 +0200

    rubocop settings, ruby version 2.6.6, update methods

--- lib/dracoon_api.rb
+++ lib/dracoon_api.rb
@@ -1,31 +1,31 @@
 # frozen_string_literal: true
 
 require_relative "dracoon_api/version"
-require 'dotenv/load'
-require 'json'
-require 'rest-client'
+require "dotenv/load"
+require "json"
+require "rest-client"
 
 module DracoonApi
   class Error < StandardError; end
   # Your code goes here...
-  
-  def auth_token
+
+  def self.auth_token
     response = RestClient.post basic_url + auth_endpoint,
                                {
-                                 'login' => ENV['DRACOON_LOGIN'],
-                                 'password' => ENV['DRACOON_PASSWORD'],
-                                 'authType' => 'sql'
+                                 "login" => ENV["DRACOON_LOGIN"],
+                                 "password" => ENV["DRACOON_PASSWORD"],
+                                 "authType" => "sql"
                                }.to_json, { content_type: :json, accept: :json }
-    @auth_token ||= JSON.parse(response)['token']
+    @auth_token ||= JSON.parse(response)["token"]
   end
 
   # Dracoon-Endpoints
-  def basic_url
-    'https://mit-dataspace.lmu-klinikum.de/api/v4/'
+  def self.basic_url
+    "https://mit-dataspace.lmu-klinikum.de/api/v4/"
   end
 
-  def auth_endpoint
-    'auth/login'
+  def self.auth_endpoint
+    "auth/login"
   end
 end
 
