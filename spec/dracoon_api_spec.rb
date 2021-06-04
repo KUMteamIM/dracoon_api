991da722093c4f870754dfd7ba71be54938ac89b
Pradeep Jerome <prjerome@gmail.com>
2021-06-04 15:55:13 +0200

    simplify test descriptions

--- spec/dracoon_api_spec.rb
+++ spec/dracoon_api_spec.rb
@@ -7,24 +7,29 @@ RSpec.describe DracoonApi do
     expect(DracoonApi::VERSION).not_to be nil
   end
 
-  it "def auth_token delivers, with valid input, a valid auth token" do
+  it "outputs a valid auth token" do
     expect(DracoonApi.auth_token(ENV["DRACOON_LOGIN"], ENV["DRACOON_PASSWORD"])).to match(/([A-Z])\w/)
   end
 
-  it "basic_get_request, with valid login, endpoint and no options, returns valid output" do
+  it "gets successful responses to GET requests" do
     response = DracoonApi.basic_get_request(ENV["DRACOON_LOGIN"], ENV["DRACOON_PASSWORD"], "nodes")
     expect(response).to include("items")
   end
 
-  it "basic_post_request, with valid login, endpoint and options, returns valid output" do
+  it "gets successful responses to POSTS requests" do
     # if successful, response is empty
     expect(DracoonApi.basic_post_request(ENV["DRACOON_LOGIN"], ENV["DRACOON_PASSWORD"], "auth/reset_password", {
                                            userName: ENV["DRACOON_LOGIN"]
                                          })).to match("")
   end
 
-  it "able to download file" do
+  it "is able to download files" do
     response =  DracoonApi.create_singular_file_download(ENV["DRACOON_LOGIN"], ENV["DRACOON_PASSWORD"], 1089)
     expect(response).to be_kind_of(RestClient::RawResponse)
   end
+
+  it "is able to create download link" do
+    
+  end
+  
 end
