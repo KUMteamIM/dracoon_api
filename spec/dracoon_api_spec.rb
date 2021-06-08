4e39e23a09bd441d2bb5828403bc7339709cce61
Pradeep Jerome <prjerome@gmail.com>
2021-06-08 12:03:16 +0200

    future proofing download link test

--- spec/dracoon_api_spec.rb
+++ spec/dracoon_api_spec.rb
@@ -31,7 +31,8 @@ RSpec.describe DracoonApi do
   end
 
   it "is able to create download link" do
-    response = DracoonApi.create_download_link(ENV["DRACOON_LOGIN"], ENV["DRACOON_PASSWORD"], ENV["FILE_ID"],"2021-07-08T09:01:14.080Z")
+    #expire in year 3000
+    response = DracoonApi.create_download_link(ENV["DRACOON_LOGIN"], ENV["DRACOON_PASSWORD"], ENV["FILE_ID"], "3000-07-08T09:01:14.080Z")
     expect(response).to match(/([A-Z])\w/)
   end
 end
